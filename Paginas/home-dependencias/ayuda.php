32903fd17d2305651eb3491599f2e8ab383c2f34
LeanSpinardi <93681816+LeanSpinardi@users.noreply.github.com>
2022-06-30 17:00:26 -0300

    Reemplazado en las paginas 'home' de los "head"s por el componente "header-home"

--- Paginas/home-dependencias/ayuda.php
+++ Paginas/home-dependencias/ayuda.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-      <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="./../../styles/style-general.css">
-      <script src="https://kit.fontawesome.com/dd947ab136.js" crossorigin="anonymous"></script>
-    </head>
+<?php
+require_once('../../Componentes/header-home.html')
+?>
     <body>
         <!--COMPONENTE barra de Navegacion-->
         <?php 
